56cd04d67ca37a8c87e3561d778ac0c2f2e23998
visualstudio <visualstudio@230-PC41>
2019-03-07 16:23:21 -0500

    lab 9

--- CSD3354-2-Assignment- 2/Program.cs
+++ CSD3354-2-Assignment- 2/Program.cs
@@ -7,28 +7,9 @@ using System.Threading.Tasks;
 namespace CSD3354_2_Assignment__2
     //ramandeep singh c0732140
 {
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            DelegateExcercises delegateExcercises = new DelegateExcercises();
-            delegateExcercises.Method3();
-            Console.ReadLine();
-        }
-    }
     public delegate void MyDelegate();
-
-    public class DelegateExcercises
+    public class DelegateExcercises : MyDelegate
     {
-        void Method1()
-        {
-            System.Console.WriteLine("Method1");
-        }
-        public void Method3()
-        {
-            MyDelegate myDelegate = new MyDelegate(Method1);
-            myDelegate();
-        }
 
     }
 }
